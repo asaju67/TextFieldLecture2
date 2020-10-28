9537f0c0dff3a5b02b82a80bf416eeea532b0323
Ashley Saju <asaju2363@stu.d214.org>
2020-10-28 12:43:07 -0700

    Initial Commit

---
+++ TextFieldLecture2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TextFieldLecture2
+//
+//  Created by user172370 on 10/28/20.
+//  Copyright © 2020 user172370. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TextFieldLecture2Tests/TextFieldLecture2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  TextFieldLecture2Tests.swift
+//  TextFieldLecture2Tests
+//
+//  Created by user172370 on 10/28/20.
+//  Copyright © 2020 user172370. All rights reserved.
+//
+
+import XCTest
+@testable import TextFieldLecture2
+
+class TextFieldLecture2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
